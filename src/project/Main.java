69d655e9ba98e79668a3349febd5edad704af7fc
AAA <temporary@email.com>
2024-10-25 17:04:33 +0900

    끝말잇기 미완성

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,12 +1,13 @@
 package project;
 
 import java.util.Scanner;
+import YJ.*;
 
 public class Main {
 	public static void main(String[] args) {
 		Scanner sc = new Scanner(System.in);
 		System.out.println("미니 게임 천국에 오신걸 환영합니다.\n메뉴를 선택해주세요.");
-		System.out.println("1. 방준혁\n2. 허은미\n3. 박소담\n4. 김영주\n5. 나호영\n6. 프로그램 종료");
+		System.out.println("1. 방준혁\n2. 허은미\n3. 박소담\n4. 김영주\n5. 나호영\n6.프로그램 종료");
 		int num = sc.nextInt();
 		while(true) {
 			switch(num) {
@@ -17,12 +18,15 @@ public class Main {
 			case 3 :
 				break;
 			case 4 :
+				Yj_DBService game= new Yj_DBServiceImpl();
+				game.display();
 				break;
 			case 5 :
 				break;
 			case 6 :
 				System.out.println("프로그램 종료");
 				return;
+				
 			}
 		}
 	}
