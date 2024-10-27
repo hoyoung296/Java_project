8bf2a48835f65114d9946d650951fcd8c0bb90d8
hoyoung <skghdud024@naver.com>
2024-10-27 15:49:58 +0900

    수정

---
+++ src/Na/asasaa.java
@@ -0,0 +1,16 @@
+package Na;
+
+public class asasaa {
+	static int test(int a) {
+		a++;
+		return a;
+	}
+
+	public static void main(String[] args) {
+		int a=0;
+		int b=0;
+		b=test(a);
+		System.out.println(b);
+	}
+
+}
