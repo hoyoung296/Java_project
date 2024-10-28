ebb50289c33ee8e998f6d6573b3f6e6b7248b5ec
hoyoung <skghdud024@naver.com>
2024-10-28 11:32:07 +0900

    수정

--- src/project/Main.java
+++ src/project/Main.java
@@ -10,9 +10,9 @@ public class Main {
 		Service ser = new Service();
 		Scanner sc = new Scanner(System.in);
 		System.out.println("미니 게임 천국에 오신걸 환영합니다.\n메뉴를 선택해주세요.");
-		System.out.println("1. 방준혁\n2. 허은미\n3. 박소담\n4. 끝말잇기 \n5. 마피아 게임\n6. 프로그램 종료");
-		int num = sc.nextInt();
 		while (true) {
+			System.out.println("1. 방준혁\n2. 허은미\n3. 박소담\n4. 끝말잇기 \n5. 마피아 게임\n6. 프로그램 종료");
+			int num = sc.nextInt();
 			switch (num) {
 			case 1:
 				break;
