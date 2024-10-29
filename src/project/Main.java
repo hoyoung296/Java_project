4764b2305deb1cadffe0944b00d8be6d98e71000
hoyoung <skghdud024@naver.com>
2024-10-29 14:50:09 +0900

    다들 고생하셨어요 ㅎㅎ

--- src/project/Main.java
+++ src/project/Main.java
@@ -1,31 +1,24 @@
 package project;
 
 import java.util.Scanner;
-
 import Na.Service;
 import YJ.service;
 import sodam.Sodamgame_main;
-
 import bang.Bang_Service;
 import bang.Bang_ServiceImpl;
 
 public class Main {
 	public static void main(String[] args) {
-
 		Huh.Service huh = new Huh.Service();
-		YJ.service game =new service();
-
-
+		service game = new service();
 		Service ser = new Service();
 		Sodamgame_main s = new Sodamgame_main();
-		Scanner sc = new Scanner(System.in);
 		Bang_Service bs = new Bang_ServiceImpl();
+		Scanner sc = new Scanner(System.in);
 
 		System.out.println("미니 게임 천국에 오신걸 환영합니다.\n메뉴를 선택해주세요.");
-
 		while (true) {
 			System.out.println("1. 숫자 맞추기\n2. 가위바위보 게임\n3. 333 로또\n4. 끝말잇기\n5. 마피아 게임\n6. 프로그램 종료");
-
 			int num = sc.nextInt();
 			switch (num) {
 			case 1:
